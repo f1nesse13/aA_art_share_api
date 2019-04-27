fb1a2cb6241b49619a64bbd2c7631c4630d4955d
Joe Pell <jpell3130@gmail.com>
2019-04-27 00:16:00 -0400

    added custom validator to check artwork created by a user cannot be shared with that same user

--- app/models/artwork_share.rb
+++ app/models/artwork_share.rb
@@ -1,6 +1,7 @@
 class ArtworkShare < ApplicationRecord
   validates :viewer_id, :artwork_id, presence: true
   validates :artwork_id, uniqueness: { scope: :viewer_id, message: "This artwork has already been shared with the user." }
+  validate :share_with_owner
 
   belongs_to :viewer,
     foreign_key: :viewer_id,
@@ -9,4 +10,10 @@ class ArtworkShare < ApplicationRecord
   belongs_to :artwork,
     foreign_key: :artwork_id,
     class_name: "Artwork"
+
+  def share_with_owner
+    if self.viewer_id == self.artwork.artist_id
+      self.errors[:viewer_id] << "Cannot share artwork with the artist!"
+    end
+  end
 end
